dcd62b24f3ced66181f871f3bc24cef8bffb64f3
Anna Rychkova <134734366+AnnaRychkova1@users.noreply.github.com>
2024-08-05 13:20:34 +0100

    Update camperApi.js

--- src/services/camperApi.js
+++ src/services/camperApi.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://669908cb2069c438cd711199.mockapi.io',
+  
   // baseURL: 'https://run.mocky.io/v3/b8ed7fe7-df51-4b58-b2f0-0d2e32fc4b55',
 });
 
